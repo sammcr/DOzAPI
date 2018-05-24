c0b68c4a8a5d0c49795803f400453a0735b13c1e
Ramiro Solis <h1.solis.castillo.ramiro@gmail.com>
2018-05-24 12:44:31 -0600

    Se corrige duplicado de Entradas

--- public/js/single-product.js
+++ public/js/single-product.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	var url_string = window.location.href;
 	var url = new URL(url_string);
 	var product = url.searchParams.get("p");
-
+  // localStorage.clear();
 	setCartItems();
 
 	if (url.pathname.indexOf("product") >= 0){
@@ -25,7 +25,7 @@ $(document).ready(function(){
 
 				// Sets product price
 				$("#product-price").html("&euro;" + result.price);
-
+        $("ul#size-list").attr("data-product", result.category.name);
 				// Sets product sizes
 				setSizes(result);
 
@@ -43,9 +43,9 @@ $(document).ready(function(){
 		if(product.category.name === 'Rings'){
 			$('#size-list').append(`
           <div class="d-inline-block btn-group g-line-height-1_2">
-            <button type="button" class="btn btn-secondary dropdown-toggle h6 
-              align-middle g-brd-none g-color-gray-dark-v5 g-color-black--hover 
-              g-bg-transparent text-uppercase g-font-weight-300 g-font-size-16 g-pa-0 
+            <button type="button" id="selected-size" class="btn btn-secondary dropdown-toggle h6
+              align-middle g-brd-none g-color-gray-dark-v5 g-color-black--hover
+              g-bg-transparent text-uppercase g-font-weight-300 g-font-size-16 g-pa-0
               g-pl-10 g-ma-0" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               ${initSize}
             </button>
@@ -53,7 +53,7 @@ $(document).ready(function(){
             </div>
           </div>`);
 			for(var i = initSize; i < (lastSize + 1); i+=2){
-				$("#ring-sizes").append(`<a class="dropdown-item g-color-gray-dark-v4 g-font-weight-300" 
+				$("#ring-sizes").append(`<a class="dropdown-item g-color-gray-dark-v4 g-font-weight-300"
 					href="javascript:void(0)">${i}</a>`);
 			}
 		}
@@ -61,7 +61,7 @@ $(document).ready(function(){
 			for(var i = 0; i < product.sizes.length; i++){
 				$('#size-list').append(`
 					<li class="list-inline-item g-mx-15">
-	          <label class="form-check-inline u-check">
+	          <label id="selected-size" class="form-check-inline u-check">
 	            <input class="g-hidden-xs-up g-pos-abs g-top-0 g-left-0" name="${product.sizes[i]}" type="radio">
 	            <span class="d-block u-check-icon-checkbox-v4 g-brd-none g-absolute-centered--y g-left-0">
 	              ${product.sizes[i]}
@@ -81,7 +81,7 @@ $(document).ready(function(){
 		plus.click(function(){
 			value = parseInt(result.val());
 			result.val(value+1);
-		});		
+		});
 
 		minus.click(function(){
 			value = parseInt(result.val());
@@ -102,21 +102,36 @@ $(document).ready(function(){
 	  var entries = getOrCreateEntries();
 
 	  // Pushes new entry to entries
-	  entries.push(createEntry());
+    if(createEntry(entries) != undefined)
+	   entries.push(createEntry(entries));
 
 	  // Updates icon
     $('#cart-items').text(entries.length);
 
     // Sets entries to localStorage updated
+
 	  localStorage.setItem('entries', JSON.stringify(entries));
+    console.log(localStorage.getItem('entries'));
   });
 
-	function createEntry(){
-	  var size = 'S';
-	  var qty = $('.js-quantity input').val();
+	function createEntry(entries){
+    if($("#size-list").attr("data-product") == "Rings"){
+        var size = parseInt($("#selected-size").text());
+    }else{
+        var size = $(".select #selected-size input").attr("name");
+    }
+	  var qty = parseInt($('.js-quantity input').val());
+	  var name_elem = $('#product-name').text();
+    var element_entrie = getElement(entries, size);
+
+    if(element_entrie.length > 0){
+      entries[element_entrie[0]].quantity += qty;
+      return undefined;
+    }
 
 	  var entry = {
 	    product_id: product,
+      name: name_elem,
       quantity: qty,
       selectedSize: size
     };
@@ -124,9 +139,18 @@ $(document).ready(function(){
     return entry;
   }
 
+  function getElement(data, size) {
+      var index_item = [];
+      data.forEach(function(item, index){
+        if(item.product_id == product  && item.selectedSize == size)
+          index_item.push(index);
+      });
+      return index_item;
+  }
+
 	function getOrCreateEntries(){
-	  var entries;
     // Gets entries from localStorage if exists
+    var entries;
     if(localStorage.getItem('entries')){
       entries = JSON.parse(localStorage.getItem('entries'));
     }
@@ -137,10 +161,18 @@ $(document).ready(function(){
   }
 
   function setCartItems() {
-    var entries = JSON.parse(localStorage.getItem('entries'));
+    console.log(localStorage.getItem('entries'));
+    if(localStorage.getItem('entries') != null)
+      var entries = JSON.parse(localStorage.getItem('entries'));
+
     if(entries)
       $('#cart-items').text(entries.length);
   }
+
+  $(document).on("click", ".list-inline-item", function(){
+    $(this).addClass("select");
+    $(this).siblings().removeClass("select");
+  });
 
 });
 
